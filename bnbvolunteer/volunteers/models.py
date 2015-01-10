80e0e9fc3cc2e74d67b043c00e81ab7402da27ca
Brittney Johnson <bjohns@mit.edu>
2015-01-10 18:16:51 -0500

    woot woot working form

--- bnbvolunteer/volunteers/models.py
+++ bnbvolunteer/volunteers/models.py
@@ -2,8 +2,8 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Userlog(models.Model):
-    user = models.ForeignKey(User)
-    date = models.DateField()
+    user = models.ForeignKey(User, default='')
+    date = models.CharField(max_length=200)
     task = models.CharField(max_length=200)
     hours = models.PositiveSmallIntegerField(default=0)
     rate = models.PositiveSmallIntegerField(default=2)
--- bnbvolunteer/volunteers/views.py
+++ bnbvolunteer/volunteers/views.py
@@ -1,16 +1,34 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import RequestContext
+from django.shortcuts import render_to_response
+from django.contrib.auth import authenticate
+from volunteers.models import *
+
 
 def volunteerHome(request):
+    csrfContext = RequestContext(request)
+    print "um"
     return render(request,'volunteerHome.html')
 
 def volunteerSubmit(request):
+    print "hey"
+    try:
+        user = authenticate(username='admin', password='adMIN')
+    except:
+        print "ERROR UGHHH"
     date = request.POST['date']
     task = request.POST['task']
     hours = request.POST['hours']
     rate = request.POST['rate']
     earned = request.POST['earned']
-    log = Userlog(user=request.user,date=date,task=task,hours=hours,rate=rate,earned=earned)
-    log.save()
-    return render(RequestContext(request),'volunteerHome.html')
+    userlog = Userlog(user=user,date=date,task=task,hours=hours,rate=rate,voucherearned=earned) #request.user
+    try: 
+        userlog.save()
+    except:
+        print "ERROR"
+    # return render(RequestContext(request),'volunteerHome.html')
+    csrfContext = RequestContext(request)
+    return render(request,'volunteerHome.html')
 
 
