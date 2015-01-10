b635a9f74b3960a642926802dffba74fe9574cf7
Brittney Johnson <bjohns@mit.edu>
2015-01-10 13:57:19 -0500

    Added basic workflow for model and volunteer home page

--- bnbvolunteer/bnbvolunteer/urls.py
+++ bnbvolunteer/bnbvolunteer/urls.py
@@ -1,5 +1,8 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+import settings 
+from volunteers import views
+
 
 urlpatterns = patterns('',
     # Examples:
@@ -7,4 +10,5 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^volunteer/home/$', views.volunteerHome, name="volunteerHome"),
 )
--- bnbvolunteer/volunteers/views.py
+++ bnbvolunteer/volunteers/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
-# Create your views here.
+def volunteerHome(request):
+    return render(request,'volunteerHome.html')
+
