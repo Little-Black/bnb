9aad9b1522d95e7a3dacb055df9bb9c5057a2be4
Brittney Johnson <bjohns@mit.edu>
2015-01-10 14:40:49 -0500

    started adding form to volunteerHome

--- bnbvolunteer/bnbvolunteer/urls.py
+++ bnbvolunteer/bnbvolunteer/urls.py
@@ -11,4 +11,5 @@ urlpatterns = patterns('',
 
     url(r'^admin/', include(admin.site.urls)),
     url(r'^volunteer/home/$', views.volunteerHome, name="volunteerHome"),
+    url(r'^volunteer/home/$', views.volunteerSubmit, name="submitNewLog"),
 )
