66a8f84627942f233f58b9d02a2da75b3160732c
Little-Black <lfzhang@mit.edu>
2015-01-15 16:33:11 -0500

    Merge

--- bnbvolunteer/volunteers/migrations/0014_merge.py
+++ bnbvolunteer/volunteers/migrations/0014_merge.py
@@ -7,11 +7,7 @@ from django.db import models, migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-<<<<<<< HEAD
         ('volunteers', '0013_userprofile_credit'),
-=======
-        ('volunteers', '0013_auto_20150114_1334'),
->>>>>>> 58897b5383fe124cb13f1d36774c44727e591ae0
         ('volunteers', '0009_auto_20150114_1600'),
     ]
 
--- bnbvolunteer/volunteers/migrations/0015_auto_20150115_1631.py
+++ bnbvolunteer/volunteers/migrations/0015_auto_20150115_1631.py
@@ -2,6 +2,7 @@
 from __future__ import unicode_literals
 
 from django.db import models, migrations
+import datetime
 
 
 class Migration(migrations.Migration):
@@ -14,13 +15,19 @@ class Migration(migrations.Migration):
         migrations.AlterField(
             model_name='activity',
             name='dateDone',
-            field=models.DateField(),
+            field=models.DateField(default=datetime.date.today),
             preserve_default=True,
         ),
         migrations.AlterField(
             model_name='activity',
             name='dateEntered',
             field=models.DateTimeField(auto_now_add=True),
+            preserve_default=True,
+        ),
+        migrations.AlterField(
+            model_name='voucher',
+            name='redemptionActivity',
+            field=models.ForeignKey(blank=True, to='volunteers.Activity', null=True),
             preserve_default=True,
         ),
     ]
