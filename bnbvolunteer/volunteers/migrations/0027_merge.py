b0d091a6ca6e84669d199a6ce86d95cd78306ad6
chsamlee <chsamlee@gmail.com>
2015-01-28 15:32:16 -0500

    phone field added to registration page

---
+++ bnbvolunteer/volunteers/migrations/0027_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('volunteers', '0026_auto_20150128_1246'),
+        ('volunteers', '0026_auto_20150128_1317'),
+    ]
+
+    operations = [
+    ]
